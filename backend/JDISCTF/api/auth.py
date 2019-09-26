13b8e4421767ef161d007564c7e5f5c1684b5280
carm2649 <maxime.carbonneau2@usherbrooke.ca>
2019-09-26 13:01:28 -0400

    Apply suggestion to backend/JDISCTF/api/auth.py

--- backend/JDISCTF/api/auth.py
+++ backend/JDISCTF/api/auth.py
@@ -20,7 +20,7 @@ def register():
     user = User.query.filter_by(email=email).first()
     
     if user is not None:
-        raise errors.Conflict("An user with that email already exists")
+        raise errors.Conflict("A user with that email already exists")
 
     user = User.query.filter_by(username=username).first()
     
