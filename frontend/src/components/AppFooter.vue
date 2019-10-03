153a112a2d8ee6476bc9bbd9d3c47eff3f286e64
gone2201 <emilio.gonzalez@usherbrooke.ca>
2019-10-03 10:43:23 -0400

    Add footer

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<!-- footer component, always present -->
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <div class="is-size-7">{{ $t('projectName') }} - 2019-2019 - Source</div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AppFooter',
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
