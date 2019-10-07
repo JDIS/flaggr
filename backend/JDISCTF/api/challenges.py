fdc4e084fab16c54bef134d91191d0a90549d0a0
Étienne Lévesque <etienne.levesque16@gmail.com>
2019-10-07 11:23:22 -0400

    Added docstring, fixed event model __repr__

--- backend/JDISCTF/api/challenges.py
+++ backend/JDISCTF/api/challenges.py
@@ -1,3 +1,5 @@
+"""Challenges routes"""
+
 from JDISCTF.app import REGISTRY
 from JDISCTF.schemas import ChallengeSchema
 from JDISCTF.models import Challenge, Category, Event
@@ -9,6 +11,7 @@ from JDISCTF.models import Challenge, Category, Event
     response_body_schema=ChallengeSchema(many=True)
 )
 def get_all_challenges_for_event(event_id: int):
+    """Get all the challenges for a given event"""
     challenges = Challenge.query.join(Category).join(Event).filter(Event.id == event_id).all()
 
     return challenges
