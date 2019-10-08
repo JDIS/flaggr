9fe438655005a7735a1db39c169aeebb67ce2662
Étienne Lévesque <etienne.levesque16@gmail.com>
2019-10-08 09:59:02 -0400

    Get challenge by id

--- backend/JDISCTF/api/challenges.py
+++ backend/JDISCTF/api/challenges.py
@@ -1,12 +1,14 @@
 """Challenges routes"""
 
+import flask_rebar
+from flask_rebar import errors
 from JDISCTF.app import REGISTRY
 from JDISCTF.schemas import ChallengeSchema
 from JDISCTF.models import Challenge, Category, Event
 
 
 @REGISTRY.handles(
-    rule="/challenges/<int:event_id>",
+    rule="/challenges/event/<int:event_id>",
     method="GET",
     response_body_schema=ChallengeSchema(many=True)
 )
@@ -15,3 +17,18 @@ def get_all_challenges_for_event(event_id: int):
     challenges = Challenge.query.join(Category).join(Event).filter(Event.id == event_id).all()
 
     return challenges
+
+
+@REGISTRY.handles(
+    rule="/challenges/<int:challenge_id>",
+    method="GET",
+    response_body_schema=ChallengeSchema
+)
+def get_challenge(challenge_id: int):
+    """Get a single challenge by its id"""
+    challenge = Challenge.query.filter_by(id=challenge_id).first()
+
+    if challenge is None:
+        raise errors.NotFound()
+
+    return challenge
