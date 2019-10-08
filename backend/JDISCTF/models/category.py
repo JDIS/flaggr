c4395d36fecb449cfb368e49d5465390427e8c4a
Étienne Lévesque <etienne.levesque16@gmail.com>
2019-10-08 10:37:26 -0400

    Category name no longer unique, (event_id, name) now is instead

--- backend/JDISCTF/models/category.py
+++ backend/JDISCTF/models/category.py
@@ -1,7 +1,7 @@
 """'Categories' SQLAlchemy model"""
 
 from JDISCTF.app import DB
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 
 
 class Category(DB.Model):
@@ -10,7 +10,12 @@ class Category(DB.Model):
     __tablename__ = 'Categories'
 
     event_id = DB.Column(DB.Integer, ForeignKey('Events.id'), nullable=True)
-    name = DB.Column(DB.String(64), index=True, unique=True)
+    name = DB.Column(DB.String(64), index=True)
+
+    __table_args__ = \
+        (
+            UniqueConstraint('event_id', 'name', name='_event_name_uc'),
+        )
 
     def __repr__(self):
         return '<Category id:{} event_id:{} name:{}'.format(self.id, self.event_id, self.name)
