b0a9157c74424ac45bcc159eab1b2bb913985258
Étienne Lévesque <etienne.levesque16@gmail.com>
2019-10-14 11:15:56 -0400

    Added seeds for categories and challenges

--- backend/seeds/seed.py
+++ backend/seeds/seed.py
@@ -2,12 +2,14 @@
 
 from JDISCTF.app import DB
 from typing import List, Generic, TypeVar
-from seeds import events
+from seeds import events, categories, challenges
 
 
 def seed(verbose: bool = False):
     """Perform seeding for all the tables"""
-    seed_table(events.get_records(), events.FILTER_ARGS, verbose)
+    db_events = seed_table(events.get_records(), events.FILTER_ARGS, verbose)
+    db_categories = seed_table(categories.get_records(db_events), categories.FILTER_ARGS, verbose)
+    seed_table(challenges.get_records(db_categories), challenges.FILTER_ARGS, verbose)
 
     print('Seeding done successfully')
 
@@ -23,13 +25,13 @@ def seed_table(records: Generic[ModelType], filter_fields: [str], verbose: bool 
         filter_args = {field_name: getattr(record, field_name) for field_name in filter_fields}
         rec = DB.session.query(record.__class__).filter_by(**filter_args).first()
         if rec:
+            record.id = rec.id
             if verbose:
                 print('Record already present: ', record)
-            record.id = rec.id
         else:
+            DB.session.add(record)
             if verbose:
                 print('Added record: ', record)
-            DB.session.add(record)
 
     DB.session.commit()
     return records
