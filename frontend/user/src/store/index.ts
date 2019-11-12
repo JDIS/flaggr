7c63c10f3680d53cb5e7027d6ad9e2b2f0a97a56
Émilio "1337 H4x0r" Gonzalez <emilio.gonzalez@usherbrooke.ca>
2019-11-12 09:36:23 -0500

    add team store

--- frontend/user/src/store/index.ts
+++ frontend/user/src/store/index.ts
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from '@/store/user'
 import network from '@/store/network'
+import team from '@/store/team'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     user,
-    network
+    network,
+    team
   },
 })
---
+++ frontend/user/src/store/team.ts
@@ -0,0 +1,49 @@
+import axios, { AxiosError, AxiosResponse } from 'axios'
+import { Team } from '@/models/team'
+
+/**
+ * Store to manage the connected user's team.
+ */
+
+const state = {
+  userTeam: null,
+}
+
+const mutations = {
+  setTeam(storeState: any, team: Team) {
+    storeState.userTeam = team;
+  },
+}
+
+const getters = {
+  hasTeam: (storeState: any) => {
+    console.log('hasteam', storeState.userTeam)
+    return storeState.userTeam !== null
+  },
+}
+
+const actions = {
+  /**
+   * Fetch the team from the server and set the state accordingly.
+   * @param context vuex context
+   */
+  fetchTeam(context: any) {
+    axios.get('team')
+      .then((response: AxiosResponse<Team>) => {
+        if (JSON.stringify(response.data) !== '{}') {
+          context.commit('setTeam', response.data)
+        }
+      })
+      .catch((error: AxiosError) => {
+        context.commit('setTeam', null)
+      })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+  getters
+}
