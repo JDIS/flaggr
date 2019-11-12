be39ff040bcd3b9c0893fa39fe1eb45a9860cf7b
Émilio "1337 H4x0r" Gonzalez <emilio.gonzalez@usherbrooke.ca>
2019-11-12 09:42:24 -0500

    add basic team management modal

---
+++ frontend/user/src/components/TeamManagementModal.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="teamManagementModal container">
+    <div class="requests">
+      <h3>{{ $t('team.requests') }}</h3>
+      <hr>
+      <div v-for="request in this.team.requests" class="pendingRequest columns is-vcentered">
+        <div class="column is-8">
+          <div>{{ request.user.username }}</div>
+          <div class="is-size-7 userEmail"> {{ request.user.email }}</div>
+        </div>
+        <div class="column">
+          <b-button @click="rejectInvitation(request)" class="button" type="is-danger" icon-right="delete"></b-button>
+        </div>
+        <div class="column">
+          <b-button @click="acceptInvitation(request)" class="button" type="is-success" icon-right="check"></b-button>
+        </div>
+      </div>
+    </div>
+    <div class="members">
+      <h3>{{ $t('team.members') }}</h3>
+      <hr />
+      <div v-for="member in this.team.members">{{ member.user.username }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import { acceptInvitation, rejectInvitation } from "@/services/team.service"
+import { TeamMixin } from "@/mixins/TeamMixin"
+import { TeamJoinRequest } from "@/models/team_join_request"
+import { sendAlertWithVariables } from "@/helpers"
+import { FlaskRebarError } from "@/models/flask_rebar_error"
+import { AxiosResponse } from "axios"
+
+/**
+ * Component to manage a team (either as a captain or a simple member)
+ */
+export default Vue.extend({
+  name: 'TeamManagementModal',
+  mixins: [TeamMixin],
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    rejectInvitation(request: TeamJoinRequest) {
+      rejectInvitation(request).then((response) => {
+        sendAlertWithVariables('team.userRejected', {user: request.user.username}, {type: 'is-success'})
+      }).catch((error: AxiosResponse<FlaskRebarError>) => {
+        sendAlertWithVariables('team.rejectError', {error: error.data.message})
+      })
+    },
+
+    acceptInvitation(request: TeamJoinRequest) {
+      console.log('requesta', request)
+      acceptInvitation(request).then((response) => {
+        sendAlertWithVariables('team.userAccepted', {user: request.user.username}, {type: 'is-success'})
+      }).catch((error: AxiosResponse<FlaskRebarError>) => {
+        sendAlertWithVariables('team.acceptError', {error: error.data.message})
+      })
+    }
+  },
+  components: {
+  }
+});
+</script>
+
+<style scoped lang="scss">
+@import "../style/theme.scss";
+
+h3 {
+  font-size: 110%;
+}
+
+hr {
+  margin: 0.2rem 0 0.5rem;
+  background: $light-3;
+}
+
+/deep/ .button {
+  width: auto !important;
+}
+
+.pendingRequest {
+  padding: 0.4rem 0 0.8rem;
+
+  .userEmail {
+    color: $light-2;
+  }
+}
+</style>
---
+++ frontend/user/src/components/TeamStep1Modal.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="teamModal">
+    <b-field :label="$t('teamManagement')" custom-class="has-text-centered is-medium"></b-field>
+    <div id="top" class="columns">
+      <div
+        class="column text is-two-fifths"
+        :class="{ active: !isJoinTeam }"
+        id="create"
+        @click="focusCreateTeam()"
+      >{{ $t('create') }}</div>
+      <b-switch
+        class="column is-flex is-one-fifth"
+        id="switch"
+        v-model="isJoinTeam"
+        :rounded="false"
+      ></b-switch>
+      <div
+        class="column text is-two-fifths has-text-right"
+        :class="{ active: isJoinTeam }"
+        id="join"
+        @click="focusJoinTeam()"
+      >{{ $t('join') }}</div>
+    </div>
+    <div id="form">
+      <b-field
+        :label="$t('teamName')"
+        custom-class="is-size-7"
+        v-if="!isJoinTeam"
+        label-for="teamName"
+      >
+        <b-input
+          placeholder="TheBestTeam"
+          expanded
+          v-model="teamName"
+          id="teamName"
+          icon="account-group"
+        ></b-input>
+      </b-field>
+      <b-field
+        :label="$t('teamToJoin')"
+        custom-class="is-size-7"
+        v-if="isJoinTeam"
+        label-for="teamToJoin"
+      >
+        <b-select
+          placeholder="TheBestTeam"
+          expanded
+          v-model="teamToJoin"
+          id="teamToJoin"
+          icon="account-group"
+        >
+          <option
+            class="has-text-light"
+            v-for="team in teams"
+            :value="team"
+            :key="team.id"
+          >{{ team.name }}</option>
+        </b-select>
+      </b-field>
+      <b-button
+        type="is-primary"
+        size="is-medium"
+        @click="onSubmit()"
+        :disabled="!canSubmit()"
+        :icon-right="isJoinTeam ? 'arrow-right' : 'plus-box'"
+      >{{ $t(isJoinTeam ? 'joinTeam' : 'createTeam') }}</b-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import { createTeam, fetchTeams, sendJoinTeamRequest } from "@/services/team.service"
+import { sendAlert, sendAlertWithVariables } from "@/helpers"
+import { AxiosResponse } from "axios"
+import { FlaskRebarError } from "@/models/flask_rebar_error"
+import { Team } from "@/models/team"
+
+/**
+ * Modal to create or join a team as a user.
+ */
+export default Vue.extend({
+  name: 'TeamStep1Modal',
+  data() {
+    return {
+      isJoinTeam: true,
+      teamName: '',
+      teamToJoin: null as (null | Team),
+      teams: [] as Team[],
+    }
+  },
+  mounted() {
+    fetchTeams().then((teams) => {
+      this.teams = teams
+    })
+  },
+  methods: {
+    focusCreateTeam() {
+      this.isJoinTeam = false
+    },
+
+    focusJoinTeam() {
+      this.isJoinTeam = true
+    },
+
+    onSubmit() {
+      if (this.isJoinTeam) {
+        if (this.teamToJoin) {
+          sendJoinTeamRequest(this.teamToJoin.id).then((response) => {
+            sendAlert('team.sendJoinRequestSent', {type: 'is-success'})
+          }).catch((error: AxiosResponse<FlaskRebarError>) => {
+            sendAlertWithVariables('team.sendJoinRequestError', {error: error.data.message})
+          })
+        }
+      } else {
+        createTeam(this.teamName).then((team) => {
+          sendAlertWithVariables('team.creationSuccessful', {team: team.name}, {type: 'is-success'})
+        }).catch((error: AxiosResponse<FlaskRebarError>) => {
+          sendAlertWithVariables('team.creationError', {error: error.data.message})
+        })
+      }
+    },
+
+    canSubmit() {
+      return (this.isJoinTeam && this.teamToJoin !== null) || (!this.isJoinTeam && this.teamName.trim() !== '')
+    }
+  },
+  components: {
+  }
+});
+</script>
+
+<style scoped lang="scss">
+@import "../style/theme.scss";
+
+#top {
+  #switch {
+    justify-content: center;
+  }
+
+  .active {
+    color: $primary-color-light;
+    text-decoration: underline;
+  }
+
+  .text {
+    font-size: 1.2rem;
+    cursor: pointer;
+  }
+
+  #connection {
+    text-align: left;
+  }
+
+  #signUp {
+    text-align: right;
+  }
+}
+</style>
