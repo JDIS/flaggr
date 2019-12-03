fb25c847db8756fbe56012a1c480f957491202c3
Pourliver <pourliver@PourliverHome.local>
2019-12-03 17:53:33 -0500

    Fixed missing imports

--- backend/JDISCTF/api/__init__.py
+++ backend/JDISCTF/api/__init__.py
@@ -1,7 +1,7 @@
 """API routes for the application"""
 
 from JDISCTF.api.admin.categories import create_category, get_admin_categories
-from JDISCTF.api.admin.challenges import get_admin_challenges, get_admin_challenge
+from JDISCTF.api.admin.challenges import get_admin_challenges, get_admin_challenge, create_challenge, edit_challenge, delete_challenge
 
 from JDISCTF.api.auth import register_participant
 from JDISCTF.api.challenges import get_all_challenges_by_category_for_event, \
