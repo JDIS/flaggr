c5245ba57e6fd6d65a5c413be4f9fa795a52cedf
Émilio Gonzalez <egg997@gmail.com>
2019-12-16 01:15:20 -0500

    fix tests

--- backend/JDISCTF/test/unit_tests/api/test_admin_event.py
+++ backend/JDISCTF/test/unit_tests/api/test_admin_event.py
@@ -1,10 +1,11 @@
+import inspect
 from unittest.mock import MagicMock, patch
 
 from flask_rebar import errors
 from pytest import fixture, raises
 
 from JDISCTF.api.admin import events
-from JDISCTF.models import Event
+from JDISCTF.models import Administrator, Event
 
 
 def local_patch(module: str):
@@ -35,11 +36,12 @@ def current_user_mock():
         yield mock
 
 A_EVENT = Event(id=0, name="Test Event", teams=True, is_open=False)
+AN_ADMINISTRATOR = Administrator(id=0, is_platform_admin=True, user_id=0)
 
 class TestCreateEvent:
-    REQUEST_BODY = {"name": "Test Event", "teams": True, "is_open": False, "is_visible": False, "front_page": "", "url": "", "flag_format": ""}
+    REQUEST_BODY = {"name": "Test Event", "teams": True, "is_open": False, "is_visible": False, "front_page": "", "flag_format": ""}
     A_NEW_EVENT = Event(name=REQUEST_BODY["name"], teams=REQUEST_BODY["teams"], is_open=REQUEST_BODY["is_open"], is_visible=REQUEST_BODY["is_visible"],
-                        front_page=REQUEST_BODY["front_page"], url=REQUEST_BODY["url"], flag_format=REQUEST_BODY["flag_format"])
+                        front_page=REQUEST_BODY["front_page"], flag_format=REQUEST_BODY["flag_format"])
 
     @fixture(autouse=True)
     def _rebar_mock(self, rebar_mock: MagicMock):
@@ -55,12 +57,13 @@ class TestCreateEvent:
         event_mock.query.filter_by.return_value.first.return_value = A_EVENT
 
         with raises(errors.UnprocessableEntity):
-            events.create_event()
+            inspect.unwrap(events.create_event)(AN_ADMINISTRATOR)
+
 
     def test_should_create_event(self, db_mock: MagicMock, event_mock: MagicMock):
         event_mock.query.filter_by.return_value.first.return_value = None
 
-        events.create_event()
+        inspect.unwrap(events.create_event)(AN_ADMINISTRATOR)
 
         db_mock.session.add.assert_called_with(self.A_NEW_EVENT)
         db_mock.session.commit.assert_called_once()
